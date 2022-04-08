b532086cd74d8184a622dbd1b2cfcd3d52b9ccf8
Vivian Huang <yvh4@cornell.edu>
2022-04-08 19:39:30 -0400

    py vers of auto and manual jupyter notebook

--- frontend.py
+++ frontend.py
@@ -34,6 +34,9 @@ small_font = pygame.font.SysFont("Helvetica neue", 25)
 # In[4]:
 
 
+manual_statement = small_font.render("Press 'M' for Manual Wordle", True, black)
+automatic_statement = small_font.render("Press 'A' for AI Wordle", True, black)
+
 win_statement = font.render("Congrats, You Win!", True, green)
 lose_statement = font.render("You Lost :(", True, red)
 replay_statement = small_font.render("Press RETURN to Play Again!", True, green)
@@ -78,22 +81,30 @@ def checkGuess(turns, answer, guess, window):
 # In[6]:
 
 
-def main():
+def manual(width, height, main_fps, main_clock, main_window):
     file = open("wordle_solutions.txt","r")
     wordle_solutions = file.readlines()
     actual_word = wordle_solutions[random.randint(0, len(wordle_solutions)-1)].upper()
     
-    SCREEN_WIDTH = 500    
-    SCREEN_HEIGHT = 600
+#     SCREEN_WIDTH = 500    
+#     SCREEN_HEIGHT = 600
 
-    FPS=30
-    clock = pygame.time.Clock()
+#     FPS=30
+#     clock = pygame.time.Clock()
     
-    window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+#     window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    SCREEN_WIDTH = width
+    SCREEN_HEIGHT = height
+    FPS = main_fps
+    clock = main_clock
+    window = main_window
+    
     window.fill(white)
     
-    guess = ""
-    
+    manual_guess = ""
+#     print("initializing guess with zero length")
+#     print(len(manual_guess))
     print(actual_word)
     
     word_length = 5
@@ -103,7 +114,7 @@ def main():
             sqr_graphic = pygame.Rect(60 + 80*x, 50 + (80*y), 50, 50)
             pygame.draw.rect(window, gray, sqr_graphic, 2) #mode 2: unfilled rectangles
             
-    pygame.display.set_caption("WORDLE")
+    pygame.display.set_caption("MANUAL WORDLE")
     
     turns = 0
     win = False
@@ -116,36 +127,40 @@ def main():
             
             #user has typed in a letter/pressed key
             if event.type == KEYDOWN:
-                guess += event.unicode.upper()
+                #only allowing alphabet options
+                if event.key == pygame.K_a or event.key == pygame.K_b or event.key == pygame.K_c or event.key == pygame.K_d or event.key == pygame.K_e or event.key == pygame.K_f or event.key == pygame.K_g or event.key == pygame.K_h or event.key == pygame.K_i or event.key == pygame.K_j or event.key == pygame.K_k or event.key == pygame.K_l or event.key == pygame.K_m or event.key == pygame.K_n or event.key == pygame.K_o or event.key == pygame.K_p or event.key == pygame.K_q or event.key == pygame.K_r or event.key == pygame.K_s or event.key == pygame.K_t or event.key == pygame.K_u or event.key == pygame.K_v or event.key == pygame.K_w or event.key == pygame.K_x or event.key == pygame.K_y or event.key == pygame.K_z:
+                    manual_guess += event.unicode.upper()
+#                 print("adding character")
+#                 print(len(guess))
                 
                 #won game
                 if event.key == K_RETURN and win == True:
-                    main()
+                    manual(SCREEN_WIDTH, SCREEN_HEIGHT, FPS, clock, window)
                 
                 #ran out of turns
                 if event.key == K_RETURN and turns > 5:
-                    main()
+                    manual(SCREEN_WIDTH, SCREEN_HEIGHT, FPS, clock, window)
                 
                 #user is typing more than five letters
-                if len(guess) > 5:
-                    guess = guess[0:5]
+                if len(manual_guess) > 5:
+                    manual_guess = manual_guess[0:5]
                 
                 #user backspaced
                 if event.key == K_BACKSPACE:
                     #removing last one
-                    guess = guess[:-1]
+                    manual_guess = manual_guess[:-1]
                     
-                if event.key == K_RETURN and len(guess) > 4:
-                    win = checkGuess(turns, actual_word, guess, window)
+                if event.key == K_RETURN and len(manual_guess) > 4:
+                    win = checkGuess(turns, actual_word, manual_guess, window)
                     turns += 1
                     
                     #restart guess
-                    guess = ""
+                    manual_guess = ""
                     #black out bottom of screen where guess was previously
                     window.fill(white, (0, 500, 500, 200))
                     
         window.fill(white, (0, 500, 500, 200))
-        guess_graphic = font.render(guess, True, black)
+        guess_graphic = font.render(manual_guess, True, black)
         window.blit(guess_graphic, (180, 530))
         
         #end game text
@@ -178,12 +193,125 @@ def main():
 # In[7]:
 
 
+#currently untested
+def automatic(guess):
+    file = open("wordle_solutions.txt","r")
+    wordle_solutions = file.readlines()
+    actual_word = wordle_solutions[random.randint(0, len(wordle_solutions)-1)].upper()
+    
+    SCREEN_WIDTH = 500    
+    SCREEN_HEIGHT = 600
+
+    FPS=30
+    clock = pygame.time.Clock()
+    
+    window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    window.fill(white)
+    
+    ai_guess = guess
+    
+    word_length = 5
+    num_guesses = 6
+    for x in range(word_length):
+        for y in range(num_guesses):
+            sqr_graphic = pygame.Rect(60 + 80*x, 50 + (80*y), 50, 50)
+            pygame.draw.rect(window, gray, sqr_graphic, 2) #mode 2: unfilled rectangles
+            
+    pygame.display.set_caption("AI WORDLE")
+    
+    turns = 0
+    win = False
+    
+    while True:
+        
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            
+        if win == True:
+            automatic(guess)
+            
+        if turns > 5:
+            automatic(guess)
+            
+        if len(ai_guess) > 4:
+            win = checkGuess(turns, actual_word, ai_guess, window)
+            turns += 1
+            ai_guess = ""
+            #black out bottom of screen where guess was previously
+            window.fill(white, (0, 500, 500, 200))
+            
+        window.fill(white, (0, 500, 500, 200))
+        guess_graphic = font.render(ai_guess, True, black)
+        window.blit(guess_graphic, (180, 530))
+        
+        if win == True:
+            #black out entire screen
+            window.fill(white)
+            
+            win_pos = win_statement.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2-50))  
+            replay_pos = replay_statement.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2+50)) 
+            
+            window.blit(win_statement, win_pos)
+            window.blit(replay_statement, replay_pos)
+        elif turns > 5 and win != True:
+#             print("you lost")
+            window.fill(white)
+            lose_pos = lose_statement.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2-50))            
+            replay_pos = replay_statement.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2+50)) 
+            window.blit(lose_statement, lose_pos)
+            window.blit(replay_statement, replay_pos)
+#             window.blit(replay_statement, (60, 300))
+            
+        pygame.display.update()
+        clock.tick(FPS)
+
+
+# In[8]:
+
+
+def main():
+    
+    SCREEN_WIDTH = 500    
+    SCREEN_HEIGHT = 600
+
+    FPS=30
+    clock = pygame.time.Clock()
+    
+    window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    window.fill(white)
+    
+    manual_pos = manual_statement.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2-50))  
+    auto_pos = automatic_statement.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2+50)) 
+            
+    window.blit(manual_statement, manual_pos)
+    window.blit(automatic_statement, auto_pos)
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+                
+        #user has typed in a letter/pressed key
+            if event.type == KEYDOWN:
+                if event.key == pygame.K_a:
+                #not implemented yet? where does the guess come from? does main need a parameter?
+                    automatic(guess)
+            
+                
+                if event.key == pygame.K_m:
+                    print("testing manual")
+                    manual(SCREEN_WIDTH, SCREEN_HEIGHT, FPS, clock, window)
+                
+        pygame.display.update()
+        clock.tick(FPS)
+        
+
+
+# In[9]:
+
+
 main()
-
-
-# In[ ]:
-
-
-#TODO: fix returning early + backspacing (only works when backspacing last letter)
-#TODO: center text for end statements
 
