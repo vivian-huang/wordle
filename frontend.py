70b7fd69e189fc026016b9ab57f93615f62a3f6a
Vivian Huang <yvh4@cornell.edu>
2022-04-28 01:45:14 -0400

    ai frontend using return to printout in frontend.py

--- frontend.py
+++ frontend.py
@@ -80,29 +80,17 @@ class Wordle:
     def generate_new_solution(self):
         self.solution = self.solution_set[random.randint(0, len(self.solution_set)-1)].upper()
         self.guessing_model.answer = self.solution.lower()
-        
-    def check_guess(self, turns, answer, guess, window):
-        self.try_count += 1
-        print("Your current ratio is ", self.get_stats())
 
+    def draw_word(self, guess, word_space, spacing, word_color, word_length, turns, window):
 
-        word_space = ["","","","","",""]
-        spacing = 0
-        word_color = [white, white, white, white, white]
-        word_length = 5
-        
-        for i, letter in enumerate(guess):
-            print(i, letter, guess)
-            if letter == answer[i]:
-                word_color[i] = green
-            elif letter in answer:
-                word_color[i] = yellow
-            else:
-                word_color[i] = gray
-                
-        list(guess)
-        
+        # word_space = ["","","","","",""]
+        # spacing = 0
+        # word_color = [white, white, white, white, white]
+        # word_length = 5
+
         for i in range(word_length):
+            # print("i: "+str(i))
+
             word_space[i] = font.render(guess[i], True, white) #rendering word onto screen
             #for each turn/guess, will want to draw word lower down by 80, first one is 50 from top
             #sqr is 50x50
@@ -112,11 +100,47 @@ class Wordle:
             letter_graphic = (70+spacing, 50 + turns*80)
             window.blit(word_space[i], letter_graphic)
             spacing += 80 #moves next letter/sqr to the right by 80
-        
+
+
+
+
+    def check_guess(self, turns, answer, guess, window):
+        self.try_count += 1
+        print("Your current ratio is ", self.get_stats())
+
+
+        word_space = ["","","","","",""]
+        spacing = 0
+        word_color = [white, white, white, white, white]
+        word_length = 5
+        game_turns = turns
+
+        for i, letter in enumerate(guess):
+            print(i, letter, guess)
+            if letter == answer[i]:
+                word_color[i] = green
+            elif letter in answer:
+                word_color[i] = yellow
+            else:
+                word_color[i] = gray
+
+        list(guess)
+        self.draw_word(guess, word_space, spacing, word_color, word_length, game_turns, window)
+        # for i in range(word_length):
+        #     word_space[i] = font.render(guess[i], True, white) #rendering word onto screen
+        #     #for each turn/guess, will want to draw word lower down by 80, first one is 50 from top
+        #     #sqr is 50x50
+        #     sqr_graphic = pygame.Rect(60 + spacing, 50 + (turns*80), 50, 50)
+        #     pygame.draw.rect(window, word_color[i], sqr_graphic) #if the first letter is green, the square drawn will be green, etc.
+        #     #drawing letters in rectangles
+        #     letter_graphic = (70+spacing, 50 + turns*80)
+        #     window.blit(word_space[i], letter_graphic)
+        #     spacing += 80 #moves next letter/sqr to the right by 80
+
         #win condition
         if word_color == [green, green, green, green, green]:
             return True
-        
+
     def get_stats(self):
         if self.game_count == 0:
             return self.try_count
@@ -128,40 +152,43 @@ class Wordle:
 
 def run(width, height, main_fps, main_clock, main_window, wordle_game):
     wordle_game.game_count += 1
-    
+
     SCREEN_WIDTH = width
     SCREEN_HEIGHT = height
     FPS = main_fps
     clock = main_clock
     window = main_window
-    
+
     window.fill(white)
-    
+
     manual_guess = ""
-    
+    if wordle_game.player_mode == PlayerMode.automatic:
+        ai_guess = wordle_game.guessing_model.next_guess().upper()
+
+
     print(wordle_game.solution)
-    
+
     word_length = 5
     num_guesses = 6
     for x in range(word_length):
         for y in range(num_guesses):
             sqr_graphic = pygame.Rect(60 + 80*x, 50 + (80*y), 50, 50)
             pygame.draw.rect(window, gray, sqr_graphic, 2) #mode 2: unfilled rectangles
-    
+
     if wordle_game.player_mode == PlayerMode.manual:
         pygame.display.set_caption("MANUAL MODE")
     elif wordle_game.player_mode == PlayerMode.automatic:
         pygame.display.set_caption("AI MODE")
-    
+
     turns = 0
     win = False
-    
+
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
-            
+
             if wordle_game.player_mode == PlayerMode.manual:
             #user has typed in a letter/pressed key
                 if event.type == KEYDOWN:
@@ -170,22 +197,23 @@ def run(width, height, main_fps, main_clock, main_window, wordle_game):
                         manual_guess += event.unicode.upper()
     #                 print("adding character")
     #                 print(len(guess))
-                    
+
                     #won game
                     if event.key == K_RETURN and win == True:
+                        # wordle_game.check_guess(turns, wordle_game.solution, manual_guess, window)
                         wordle_game.generate_new_solution()
 
                         run(SCREEN_WIDTH, SCREEN_HEIGHT, FPS, clock, window, wordle_game)
-                    
+
                     #ran out of turns
                     if event.key == K_RETURN and turns > 5:
                         wordle_game.generate_new_solution()
                         run(SCREEN_WIDTH, SCREEN_HEIGHT, FPS, clock, window, wordle_game)
-                    
+
                     #user is typing more than five letters
                     if len(manual_guess) > 5:
                         manual_guess = manual_guess[0:5]
-                    
+
                     #user backspaced
                     if event.key == K_BACKSPACE:
                         #removing last one
@@ -196,7 +224,7 @@ def run(width, height, main_fps, main_clock, main_window, wordle_game):
                     if event.key == K_RETURN and len(manual_guess) > 4 and manual_guess.lower() in wordle_game.possible_guesses:
                         win = wordle_game.check_guess(turns, wordle_game.solution, manual_guess, window)
                         turns += 1
-                        
+
                         #restart guess
                         manual_guess = ""
                         #black out bottom of screen where guess was previously
@@ -208,53 +236,63 @@ def run(width, height, main_fps, main_clock, main_window, wordle_game):
                 print("turns" + str(turns))
                 print(wordle_game.guessing_model.possible_guesses)
 
-                ai_guess = wordle_game.guessing_model.next_guess().upper()
+                # ai_guess = wordle_game.guessing_model.next_guess().upper()
                 print("HERE: ", ai_guess)
-                if win == True:
-                    wordle_game.generate_new_solution()
-                    wordle_game.guessing_model.possible_guesses = wordle_game.possible_guesses
-                    run(SCREEN_WIDTH, SCREEN_HEIGHT, FPS, clock, window, wordle_game)
-        
-                if turns > 5:
-                    wordle_game.generate_new_solution()
 
-                    wordle_game.guessing_model.possible_guesses = wordle_game.possible_guesses
-                    run(SCREEN_WIDTH, SCREEN_HEIGHT, FPS, clock, window, wordle_game)
+                if event.type == KEYDOWN:
 
-                
-#                 if len(ai_guess) > 4:
-                win = wordle_game.check_guess(turns, wordle_game.solution, ai_guess, window)
-                turns += 1
-                    #black out bottom of screen where guess was previously
-#                     window.fill(white, (0, 500, 500, 200))
-        
-        #showing guess at bottom for manual vers
-        window.fill(white, (0, 500, 500, 200))
-        guess_graphic = font.render(manual_guess, True, black)
-        window.blit(guess_graphic, (180, 530))
-        
+                    if event.key == K_RETURN and win == True:
+                        wordle_game.generate_new_solution()
+                        wordle_game.guessing_model.possible_guesses = wordle_game.possible_guesses
+                        run(SCREEN_WIDTH, SCREEN_HEIGHT, FPS, clock, window, wordle_game)
+
+                    #if press return and ran out of turns
+                    if event.key == K_RETURN and turns > 5:
+                        wordle_game.generate_new_solution()
+
+                        wordle_game.guessing_model.possible_guesses = wordle_game.possible_guesses
+                        run(SCREEN_WIDTH, SCREEN_HEIGHT, FPS, clock, window, wordle_game)
+
+                    if event.key == K_RETURN:
+
+    #                 if len(ai_guess) > 4:
+                        win = wordle_game.check_guess(turns, wordle_game.solution, ai_guess, window)
+                        turns += 1
+
+                        ai_guess = wordle_game.guessing_model.next_guess().upper()
+
+        if wordle_game.player_mode == PlayerMode.manual:
+            window.fill(white, (0, 500, 500, 200))
+            guess_graphic = font.render(manual_guess, True, black)
+            window.blit(guess_graphic, (180, 530))
+        elif wordle_game.player_mode == PlayerMode.automatic:
+            window.fill(white, (0, 500, 500, 200))
+            guess_graphic = font.render(ai_guess, True, black)
+            window.blit(guess_graphic, (180, 530))
+
         #end game text
 #         print("turns: " + str(turns))
 #         print(win)
         if win == True:
             #black out entire screen
             window.fill(white)
-            
-            win_pos = win_statement.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2-50))  
-            replay_pos = replay_statement.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2+50)) 
-            
+
+            win_pos = win_statement.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2-50))
+            replay_pos = replay_statement.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2+50))
+
             window.blit(win_statement, win_pos)
             window.blit(replay_statement, replay_pos)
 #             window.blit(replay_statement, (60, 300))
         elif turns > 5 and win != True:
 #             print("you lost")
             window.fill(white)
-            lose_pos = lose_statement.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2-50))            
-            replay_pos = replay_statement.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2+50)) 
+            lose_pos = lose_statement.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2-50))
+            replay_pos = replay_statement.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2+50))
             window.blit(lose_statement, lose_pos)
             window.blit(replay_statement, replay_pos)
 #             window.blit(replay_statement, (60, 300))
-            
+
+        #moved run outside
         pygame.display.update()
         clock.tick(FPS)
 
@@ -263,33 +301,33 @@ def run(width, height, main_fps, main_clock, main_window, wordle_game):
 
 
 def main():
-    
-    SCREEN_WIDTH = 500    
+
+    SCREEN_WIDTH = 500
     SCREEN_HEIGHT = 600
 
     FPS=30
     clock = pygame.time.Clock()
-    
+
     window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     window.fill(white)
-    
-    manual_pos = manual_statement.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2-50))  
-    auto_pos = automatic_statement.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2+50)) 
-            
+
+    manual_pos = manual_statement.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2-50))
+    auto_pos = automatic_statement.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2+50))
+
     window.blit(manual_statement, manual_pos)
     window.blit(automatic_statement, auto_pos)
-    
+
     pygame.display.set_caption("WORDLE")
-    
+
     file1 = open("wordle_solutions.txt","r")
     wordle_solutions = file1.readlines()
-    
+
     file2 = open("common.txt","r")
     possible_guesses = file2.read().splitlines()
 #     print("possible_guesses type:")
 #     print(type(possible_guesses))
 #     print(possible_guesses)
-    
+
 #     manual_bool = None
 
     while True:
@@ -297,7 +335,7 @@ def main():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
-                
+
         #user has typed in a letter/pressed key
             if event.type == KEYDOWN:
                 #have not yet set automatic or manual mode
@@ -306,7 +344,7 @@ def main():
                     print("testing automatic")
                     manual_bool = False
                     window.fill(white)
-#                     normal_pos = normal_statement.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2-50))  
+#                     normal_pos = normal_statement.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2-50))
 #                     hard_pos = hard_statement.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2+50))
 #                     window.blit(normal_statement, normal_pos)
 #                     window.blit(hard_statement, hard_pos)
@@ -318,13 +356,13 @@ def main():
                     print("testing manual")
                     manual_bool = True
                     window.fill(white)
-#                     normal_pos = normal_statement.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2-50))  
+#                     normal_pos = normal_statement.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2-50))
 #                     hard_pos = hard_statement.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2+50))
 #                     window.blit(normal_statement, normal_pos)
 #                     window.blit(hard_statement, hard_pos)
                     wordle_game = Wordle(GameDifficulty.normal, PlayerMode.manual, wordle_solutions, possible_guesses)
                     run(SCREEN_WIDTH, SCREEN_HEIGHT, FPS, clock, window, wordle_game)
-                        
+
                 #have set automatic/manual and now need to set normal/hard
 #                 if manual_bool is not None:
 #                     if event.key == pygame.K_n and manual_bool == True:
@@ -345,7 +383,7 @@ def main():
 #                         run(SCREEN_WIDTH, SCREEN_HEIGHT, FPS, clock, window, wordle_game)
 
 
-        
+
         pygame.display.update()
         clock.tick(FPS)
 
@@ -354,4 +392,3 @@ def main():
 
 
 main()
-
